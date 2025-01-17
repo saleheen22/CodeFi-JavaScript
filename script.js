e0410ec808d9da83c8bd54d362235f75c3098e68
Abrar Saleheen <54772944+saleheen19101331@users.noreply.github.com>
2025-01-16 18:56:57 -0600

    Done with the html and css basics

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-console.log(`I am getting better everyday, ain't I`);
-let firstName = "abrar";
-let lastName = "Saleheen";
-console.log(`My first name is ${firstName} and the lastname is ${lastName}`);
+// console.log(`I am getting better everyday, ain't I`);
+// let firstName = "abrar";
+// let lastName = "Saleheen";
+// console.log(`My first name is ${firstName} and the lastname is ${lastName}`);
+// function showModal(message) {
+//     const modal = document.createElement('div');
+//     modal.style.position = 'fixed';
+//     modal.style.top = '50%';
+//     modal.style.left = '50%';
+//     modal.style.transform = 'translate(-50%, -50%)';
+//     modal.style.padding = '20px';
+//     modal.style.backgroundColor = 'red';
+//     modal.style.boxShadow = '0 4px 8px rgba(58, 4, 4, 0.2)';
+//     modal.innerText = message;
+  
+//     document.body.appendChild(modal);
+//     setTimeout(() => document.body.removeChild(modal), 3000);
+//   }
+  
+//   showModal('Hello, this is a custom modal!');
