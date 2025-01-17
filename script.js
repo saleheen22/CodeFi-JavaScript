d6df3a516796de83d2e5aeb3bd52365dc637f261
Abrar Saleheen <54772944+saleheen19101331@users.noreply.github.com>
2025-01-16 18:13:30 -0600

    Done with string lateral

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-let a = 2;
-let b = 234;
-console.log(a+b);
+console.log(`I am getting better everyday, ain't I`);
+let firstName = "abrar";
+let lastName = "Saleheen";
+console.log(`My first name is ${firstName} and the lastname is ${lastName}`);
