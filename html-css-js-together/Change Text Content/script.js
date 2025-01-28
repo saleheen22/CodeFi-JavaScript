2b39301dcb99c80081c29060a7dbd2f7a663ee2a
Abrar Saleheen <54772944+saleheen19101331@users.noreply.github.com>
2025-01-27 19:06:36 -0600

    tchange the text color

---
+++ html-css-js-together/Change Text Content/script.js
@@ -0,0 +1,3 @@
+document.getElementById('btn-para').addEventListener('click', ()=> {
+    document.getElementById('para').innerText = 'You clicked the button'
+})
---
+++ html-css-js-together/alert-webpage/script.js
@@ -0,0 +1,1 @@
+document.getElementById('btn').addEventListener('click', alert('This is an alert'));
